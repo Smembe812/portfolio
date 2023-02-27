e0abe86468d4d64fda01ce375a1f65be2d8af4bd
smembe812 <psemberekajr@gmail.com>
2023-02-27 21:38:00 +0100

    fix(deps): update

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,31 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream')
-var sass = require('gulp-sass')
-//var less = require('gulp-less');
+var sass = require('gulp-sass')(require('node-sass'))
 var minifyCSS = require('gulp-csso');
-//var concat = require('gulp-concat');
-//var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('compile:css', function () {
+function compileStyles() {
   return gulp.src(['./src/sass/*.scss'])
     .pipe(sass())
-    //.pipe(minifyCSS())
+    .pipe(minifyCSS())
     .pipe(gulp.dest('./public/assets/css'))
-});
-
-gulp.task('compile:js', function () {
+}
+function bundleJs() {
   var bundle = browserify('./src/js/main.js').bundle();
-
   return bundle
     .pipe(source("script.js"))
     .pipe(gulp.dest("./public/assets/js/"));
-});
+}
 
-gulp.task('compile:bootstrap', function () {
+function compileBootstrap() {
   return gulp.src(['./node_modules/bootstrap/scss/**/*.scss'])
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./public/assets/css'))
-})
+}
 
-gulp.task('watch', ["compile:js", "compile:css"], function () {
-  gulp.watch(["./src/js/**/*.js"], ["compile:js"]);
-  gulp.watch(["./src/sass/**/*.scss"], ["compile:css"])
-});
+// gulp.task('watch', ["compile:js", "compile:css"], function () {
+//   gulp.watch(["./src/js/**/*.js"], ["compile:js"]);
+//   gulp.watch(["./src/sass/**/*.scss"], ["compile:css"])
+// });
+exports.build = gulp.series(compileBootstrap, compileStyles, bundleJs)
