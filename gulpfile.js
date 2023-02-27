d339b147e5e957472845c9dcd41f9fd139e5c5a6
smembe812 <psemberekajr@gmail.com>
2023-02-27 20:45:33 +0100

    chore(gulp): format

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ var minifyCSS = require('gulp-csso');
 //var concat = require('gulp-concat');
 //var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('compile:css', function(){
+gulp.task('compile:css', function () {
   return gulp.src(['./src/sass/*.scss'])
     .pipe(sass())
     //.pipe(minifyCSS())
     .pipe(gulp.dest('./public/assets/css'))
 });
 
-gulp.task('compile:js', function(){
+gulp.task('compile:js', function () {
   var bundle = browserify('./src/js/main.js').bundle();
 
   return bundle
@@ -22,14 +22,14 @@ gulp.task('compile:js', function(){
     .pipe(gulp.dest("./public/assets/js/"));
 });
 
-gulp.task('compile:bootstrap', function(){
+gulp.task('compile:bootstrap', function () {
   return gulp.src(['./node_modules/bootstrap/scss/**/*.scss'])
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(gulp.dest('./public/assets/css'))
 })
 
-gulp.task('watch', ["compile:js", "compile:css"], function(){
+gulp.task('watch', ["compile:js", "compile:css"], function () {
   gulp.watch(["./src/js/**/*.js"], ["compile:js"]);
   gulp.watch(["./src/sass/**/*.scss"], ["compile:css"])
 });
